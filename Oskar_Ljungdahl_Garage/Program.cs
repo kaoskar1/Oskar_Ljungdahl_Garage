44cc6c1fbc1db693bdae0bf4ea2c269f9cc09087
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-04-30 13:36:54 +0200

    mer uppdates

--- Oskar_Ljungdahl_Garage/Program.cs
+++ Oskar_Ljungdahl_Garage/Program.cs
@@ -16,16 +16,73 @@ namespace Oskar_Ljungdahl_Garage
 
         private static void printMenu()
         {
+
             Console.WriteLine("****GARAGE****");
-            Console.WriteLine("*****(L)ist****");
-            Console.WriteLine("List all the vehicle in the (G)arage");
-            Console.WriteLine("List vehicle (T)ypes");
+            Console.WriteLine("****(L)ist****");
+            //Console.WriteLine("List all the vehicle in the (G)arage");
+            //Console.WriteLine("List vehicle (T)ypes");
             Console.WriteLine("****(S)earch****");
-            Console.WriteLine("Search on the (N)umber plate");
-            Console.WriteLine("Search on an (A)ttribut");
+            //Console.WriteLine("Search on the (N)umber plate");
+            //Console.WriteLine("Search on (P)roperties");
             Console.WriteLine("****(E)dit****");
-            Console.WriteLine("(A)dd A Vehicle");
-            Console.WriteLine("(R)emove a Vehicle");
+            //Console.WriteLine("(A)dd A Vehicle");
+            //Console.WriteLine("(R)emove a Vehicle");
+
+            var menuMode = true;
+            do {
+
+                var key = Console.ReadKey(intercept: true).Key;
+                switch (key)
+                {
+
+                    case ConsoleKey.L:
+                        menuMode = true;
+                        Console.WriteLine("L preesed");
+                        break;
+
+                    case ConsoleKey.G:
+                        menuMode = true;
+                        Console.WriteLine("G preesed");
+                        break;
+
+                    case ConsoleKey.T:
+                        menuMode = true;
+                        Console.WriteLine("T preesed");
+                        break;
+
+                    case ConsoleKey.S:
+                        menuMode = true;
+                        Console.WriteLine("S preesed");
+                        break;
+
+                    case ConsoleKey.N:
+                        menuMode = true;
+                        Console.WriteLine("N preesed");
+                        break;
+
+                    case ConsoleKey.P:
+                        menuMode = true;
+                        Console.WriteLine("P preesed");
+                        break;
+
+                    case ConsoleKey.E:
+                        menuMode = true;
+                        Console.WriteLine("E preesed");
+                        break;
+
+                    case ConsoleKey.A:
+                        menuMode = true;
+                        Console.WriteLine("A preesed");
+                        break;
+
+                    case ConsoleKey.R:
+                        menuMode = true;
+                        Console.WriteLine("R preesed");
+                        break;
+
+                }
+            } while (menuMode);
+
         }
     }
 }
