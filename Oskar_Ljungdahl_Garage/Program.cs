9a6b2744667f8a6880adf5369e167e659cf3026c
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-04-30 12:15:54 +0200

    uppdates

--- Oskar_Ljungdahl_Garage/Program.cs
+++ Oskar_Ljungdahl_Garage/Program.cs
@@ -11,7 +11,21 @@ namespace Oskar_Ljungdahl_Garage
         static void Main(string[] args)
         {
 
-            Console.WriteLine("HAMMARBY");
+            printMenu();
+        }
+
+        private static void printMenu()
+        {
+            Console.WriteLine("****GARAGE****");
+            Console.WriteLine("*****(L)ist****");
+            Console.WriteLine("List all the vehicle in the (G)arage");
+            Console.WriteLine("List vehicle (T)ypes");
+            Console.WriteLine("****(S)earch****");
+            Console.WriteLine("Search on the (N)umber plate");
+            Console.WriteLine("Search on an (A)ttribut");
+            Console.WriteLine("****(E)dit****");
+            Console.WriteLine("(A)dd A Vehicle");
+            Console.WriteLine("(R)emove a Vehicle");
         }
     }
 }
